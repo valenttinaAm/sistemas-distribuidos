296d7c33e8dc79cfb6962936d649464f47a8e3a8
Valentina Aguirre <142761162+valenttinaAm@users.noreply.github.com>
2026-05-31 23:05:03 -0400

    Update main.py

--- consumidor_kafka/main.py
+++ consumidor_kafka/main.py
@@ -124,8 +124,3 @@ async def ejecutar_consumidor():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(ejecutar_consumidor())
-
-if                                    
-
-        a
-    a
